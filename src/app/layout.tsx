1bd0275b5a5b076a15b76b369452038a9c53d565
priyanshu kushwaha <priyanshukushwaha444@gmail..com>
2026-04-09 18:10:23 +0530

    Add Google Analytics

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Montserrat, Poppins } from "next/font/google";
 import "./globals.css";
 import { Navbar } from "@/components/navbar";
+import Script from "next/script";
 import { Footer } from "@/components/footer";
 
 const poppins = Poppins({ 
@@ -38,6 +39,20 @@ export default function RootLayout({
         <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1" />
       </head>
       <body className="antialiased min-h-screen bg-slate-50 dark:bg-slate-950 font-sans" suppressHydrationWarning>
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-82T5Z6KYGP"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-82T5Z6KYGP');
+          `}
+        </Script>
         <header>
           <Navbar />
         </header>
